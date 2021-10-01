fdb42275bd11fbc8fcadffc5fa3f2ba52565754c
rvky09 <rifqiaria95@outlook.com>
2021-10-01 17:25:37 +0700

    update lagi

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
