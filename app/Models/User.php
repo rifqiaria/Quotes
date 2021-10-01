86a91e0901b8ee72194a20e1a5f7d4aef0f506a2
rvky09 <rifqiaria95@outlook.com>
2021-10-01 16:55:21 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
